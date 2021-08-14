d786e19c9ad79f86ac3c5fc293c8716a3f63928c
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-08-15 02:31:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/covidapple/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.covidapple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        
+    }
+}
